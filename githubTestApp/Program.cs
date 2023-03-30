97eaeb106d7253c35ff9fdde9bdfb8e7b4c730e1
Thomas Tjellesen <THTJ6205@ucl.dk>
2023-03-30 12:25:55 +0200

    indsatte en forløkke

--- githubTestApp/Program.cs
+++ githubTestApp/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Dette er en test af github");
+            for (int a = 1; a <= 10; a++)
+            {
+                Console.WriteLine(a);
+            }
         }
     }
 }
